9d720fdb115de589095977ff4362a9dfdfdac010
Tommy Lewis <lewisc0021@gmail.com>
2016-12-07 11:56:09 -0500

    Changes to Yahoo Finance Class

--- MarketStumblerFinal/Models/YahooFinance.cs
+++ MarketStumblerFinal/Models/YahooFinance.cs
@@ -11,19 +11,25 @@ namespace MarketStumblerFinal.Models
         {
             List<Stock> stockInfo = new List<Stock>();
             string[] rows = csvData.Replace("\r", "").Split('\n');
+            int i = 0;
             foreach (string row in rows)
             {
                 if (string.IsNullOrEmpty(row)) continue;
                 string[] cols = row.Split(',');
-
+                // detect the problem 
+                if (cols.Length == 8)
+                {
+                    i += 1;
+                    cols[1] += ", Inc.";
+                }
                 Stock Company = new Stock();
                 Company.Symbol = cols[0];
                 Company.Name = cols[1];
-                Company.Bid = Convert.ToDecimal(cols[2]);
-                Company.Ask = Convert.ToDecimal(cols[3]);
-                Company.Open = Convert.ToDecimal(cols[4]);
-                Company.PreviousClose = Convert.ToDecimal(cols[5]);
-                Company.Last = Convert.ToDecimal(cols[6]);
+                Company.Bid = Convert.ToDecimal(cols[2+i]);
+                Company.Ask = Convert.ToDecimal(cols[3+i]);
+                Company.Open = Convert.ToDecimal(cols[4+i]);
+                Company.PreviousClose = Convert.ToDecimal(cols[5+i]);
+                Company.Last = Convert.ToDecimal(cols[6+i]);
 
                 stockInfo.Add(Company);
 
