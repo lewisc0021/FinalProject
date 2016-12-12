06991543597d750978e81eac72973a39a7ac0953
raghuma sama <raghuma sama>
2016-12-12 12:18:18 -0500

    Popup1controller and popup1 view added

---
+++ MarketStumblerFinal/Controllers/Popup1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketStumblerFinal.Controllers
+{
+    public class Popup1Controller : Controller
+    {
+        // GET: Popup1
+        public ActionResult Popup1(string Symbol)
+        {
+            ViewBag.Symbol = Symbol;
+            return View();
+        }
+    }
+}
